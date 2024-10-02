3d631a0da87f92ff4b1b56787583c616df7c100e
moham <mohmaedsalihbaher2021@gmail.com>
2024-10-02 01:11:23 -0500

    Contributing BoxDemo

---
+++ src/main/java/the_complete_referenc_chapter06_pages117_136/BoxDemo.java
@@ -0,0 +1,29 @@
+package the_complete_referenc_chapter06_pages117_136;
+
+/* A program that uses the Box class.
+
+   Call this file BoxDemo.java
+*/
+class Box {
+    double width;
+    double height;
+    double depth;
+}
+
+// This class declares an object of type Box.
+public class BoxDemo {
+    public static void main(String[] args) {
+        Box mybox = new Box();
+        double vol;
+
+        // assign values to mybox's instance variables
+        mybox.width = 10;
+        mybox.height = 20;
+        mybox.depth = 15;
+
+        // compute volume of box
+        vol = mybox.width * mybox.height * mybox.depth;
+
+        System.out.println("Volume is " + vol);
+    }
+}
