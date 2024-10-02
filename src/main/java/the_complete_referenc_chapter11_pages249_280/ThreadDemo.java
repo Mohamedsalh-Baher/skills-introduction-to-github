5cfe5f194523e3bb59015ca506d6de5f3cc10e68
moham <mohmaedsalihbaher2021@gmail.com>
2024-10-02 02:10:19 -0500

    ThreadDemo

---
+++ src/main/java/the_complete_referenc_chapter11_pages249_280/ThreadDemo.java
@@ -0,0 +1,44 @@
+package the_complete_referenc_chapter11_pages249_280;
+
+// Create a second thread.
+class NewThread implements Runnable {
+    Thread t;
+
+    NewThread() {
+        // Create a new, second thread
+        t = new Thread(this, "Demo Thread");
+        System.out.println("Child thread: " + t);
+    }
+
+    // This is the entry point for the second thread.
+    public void run() {
+        try {
+            for(int i = 5; i > 0; i--) {
+                System.out.println("Child Thread: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Child interrupted.");
+        }
+        System.out.println("Exiting child thread.");
+    }
+}
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        NewThread nt = new NewThread(); // create a new thread
+
+        nt.t.start(); // Start the thread
+
+        try {
+            for(int i = 5; i > 0; i--) {
+                System.out.println("Main Thread: " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted.");
+        }
+        System.out.println("Main thread exiting.");
+    }
+}
+
